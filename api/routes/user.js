25f76d646c0ccb66239b67dfae14056091a28355
errorbyme <amitk74019@gmail.com>
2024-10-10 21:25:04 +0530

    update

--- api/routes/user.js
+++ api/routes/user.js
@@ -143,7 +143,8 @@ router.get("/auth", (req, res) => {
 
 router.post("/logout", (req, res) => {
   console.log("Initial cookies:", req.cookies); // Log current cookies
-  res.clearCookie("token"); // Clear the cookie
+  res.clearCookie("token", { path: "/" }); // Clear the cookie
+  res.cookie("token", "", { path: "/", expires: new Date(0) });
   console.log("Cookies after clear attempt:", req.cookies); // Log again after clearing
   return res.status(201).json("ok");
 });
